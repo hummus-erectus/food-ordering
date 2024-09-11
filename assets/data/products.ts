f3235091497171b25f3629f81eb10a79a979fa6e
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-12 00:33:15 +0900

    1. Building UI

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -1,28 +1,28 @@
 const products = [
   {
     id: 1,
-    name: 'Ultimate Pepperoni',
+    name: 'Ultimate Vegan Pepperoni',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
     price: 12.99,
   },
   {
     id: 2,
-    name: 'ExtravaganZZa',
+    name: 'ExtraveganZZa',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
     price: 14.99,
   },
   {
     id: 3,
-    name: 'MeatZZa',
+    name: 'Vegan MeatZZa',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
     price: 13.47,
   },
   {
     id: 4,
-    name: 'Margarita',
+    name: 'Vegan Margarita',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
     price: 9.9,
@@ -36,35 +36,35 @@ const products = [
   },
   {
     id: 6,
-    name: 'Hawaiian',
+    name: 'Vegan Hawaiian',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
     price: 10.49,
   },
   {
     id: 7,
-    name: 'Deluxe',
+    name: 'Vegan Deluxe',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
     price: 16.99,
   },
   {
     id: 8,
-    name: 'BBQ Chicken',
+    name: 'BBQ VFC',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
     price: 12.89,
   },
   {
     id: 9,
-    name: 'Chicken Bacon Ranch',
+    name: 'Vegan Chicken Bacon Ranch',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
     price: 13.99,
   },
   {
     id: 10,
-    name: '6 Cheese',
+    name: '6 Vegan Cheese',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
     price: 13.29,
