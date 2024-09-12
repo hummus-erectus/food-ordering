04276adb432e381695e52d1ca8833581144f35e8
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-12 13:36:21 +0900

    Expo Router Screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,5 @@
-import { View } from '@/src/components/Themed';
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
-import { FlatList } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function MenuScreen() {
-  return (
-    <View>
-      {/* <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/> */}
-      <FlatList
-        data={products}
-        renderItem={({item}) => <ProductListItem product={item}/>}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
-    </View>
-  );
-}
+export default function TabIndex () {
+  return <Redirect href={'/menu/'} />;
+};
