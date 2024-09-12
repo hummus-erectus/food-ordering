c3193bdede1ad310856e6bb1050c11730387ac72
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-12 12:00:29 +0900

    Type safe components

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/two` | `/..\..\assets\data\orders` | `/..\..\assets\data\products` | `/..\components\ProductListItem` | `/_sitemap` | `/modal` | `/two`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/two` | `/_sitemap` | `/modal` | `/two`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Image } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
 import Colors from '@/src/constants/Colors';
+import { Product } from '@/src/types';
 
-const ProductListItem = ({ product }) => {
+export const defultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/default.png'
+
+type ProductListItemProps = {
+  product: Product;
+}
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
+      <Image source={{uri: product.image || defultPizzaImage}} style={styles.image}/>
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
