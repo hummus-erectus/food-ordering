56752b5ae40bb30b152b4febb5cf1a5ff952c27e
Robert Grayson <robert.samuel.grayson@gmail.com>
2024-09-12 12:21:40 +0900

    Flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
 import { View } from '@/src/components/Themed';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
+import { FlatList } from 'react-native';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
+      {/* <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/> */}
+      <FlatList
+        data={products}
+        renderItem={({item}) => <ProductListItem product={item}/>}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   );
 }
